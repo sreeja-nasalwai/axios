b64ac8b41f23aed87be2768edcfeafa6a7681a16
sreeja-nasalwai <152997004+sreeja-nasalwai@users.noreply.github.com>
2024-05-16 17:17:30 +0530

    Update ax.js

--- ax.js
+++ ax.js
@@ -1,19 +1,9 @@
 async function getData(){
-    // let id1=document.getElementById("roll").value;
-    // let name1=document.getElementById("name").value;
-    // let age1=document.getElementById("age").value;
-    // let branch1=document.getElementById("bran").value;
-
      const res = await axios.get("http://localhost:3000/students");
         const students = res.data;
 
         const tableBody = document.getElementById("studList");
         tableBody.innerHTML = ""; // Clear previous content
-
-        // Create table header
-    
-
-        // Populate table with student data
         students.forEach(student => {
             const row = document.createElement("tr");
             row.innerHTML = `<td>${student.id}</td><td>${student.name}</td><td>${student.age}</td><td>${student.branch}</td><td><i class="fa fa-trash-o" style="font-size:36px;color:red" onclick="delete1(${student.id})"></i></td> `;
